6a6b8b40497b457ba105f43b8add42afa2c559f5
ppbarros <ppbarro@sempreceub.com>
2019-04-26 18:39:26 -0300

    Tudo Funcionando, com função de inserir e deletar

--- bd.py
+++ bd.py
@@ -1,4 +1,19 @@
 def get_contatos(cursor):
-    cursor.execute(f'select nome, telefone, email from contatos')
+    cursor.execute(f'select nome, telefone, email, idcontatos from contatos')
     contatos = cursor.fetchall()
     return contatos
+
+
+def set_contato(cursor, conn, nome, tel, email):
+    cursor.execute(f'select idcontatos from contatos where nome = "{nome}" and telefone = "{tel}" and email = "{email}"')
+    sera = cursor.fetchone()
+    if sera is None:
+        cursor.execute(f'insert into contatos (nome, telefone, email) values ("{nome}", "{tel}", "{email}")')
+        conn.commit()
+    else:
+        return 'Contato já existente!'
+
+
+def delete(cursor, conn, idcontato):
+    cursor.execute(f'delete from contatos where idcontatos = {idcontato}')
+    conn.commit()
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 from flaskext.mysql import MySQL
 from bd import *
 app = Flask(__name__)
@@ -17,11 +17,42 @@ def principal():
 def contatos():
     conn = banco.connect()
     cursor = conn.cursor()
-    return render_template('contatos.html', contatos=get_contatos(cursor))
+    contatos = get_contatos(cursor)
+    cursor.close()
+    conn.close()
+    return render_template('contatos.html', contatos=contatos)
 
-@app.route('/inserir', method = ['POST'])
-def inserir_contato()
-    return
+@app.route('/inserir')
+def inserir_contato():
+    return render_template('form_cont.html')
+
+
+@app.route('/inserir_cont', methods=['POST'])
+def comfirm_inserir():
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        tele = request.form.get('tel')
+        email = request.form.get('email')
+    conn = banco.connect()
+    cursor = conn.cursor()
+    teste = set_contato(cursor, conn, nome, tele, email)
+    if teste:
+        cursor.close()
+        conn.close()
+        return render_template('form_cont.html', erro=teste)
+    cursor.close()
+    conn.close()
+    return redirect(url_for('contatos'))
+
+@app.route('/delete/<idcontato>')
+def delete_cont(idcontato):
+    conn = banco.connect()
+    cursor = conn.cursor()
+    delete(cursor, conn, idcontato)
+    cursor.close()
+    conn.close()
+    return redirect(url_for('contatos'))
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
